50fb7387492b18fd20250a9e2162633c324e80a6
Joel Eager <jee7291@rit.edu>
2017-01-27 13:35:20 -0500

    Improved client id assignment

--- wsServer.py
+++ wsServer.py
@@ -1,8 +1,8 @@
 import asyncio
 import websockets
 import datetime
+import random
 
-nextId = 0          # The next unused id
 logLevel = 0
 
 # Data structures
@@ -22,61 +22,70 @@ def timeDelta(aTime):
 
 # Appends a message to the outgoing queues for all clients
 def sendAll(message):
-    for id in outgoing:
-        outgoing[id].append(message)
+    for clientID in outgoing:
+        outgoing[clientID].append(message)
 
     logPrint("Message added to send queue: " + message)
 
 # Sends queued messages to a client
-async def sendTask(id, websocket):
-    while id in outgoing:
-        if len(outgoing[id]) != 0:
-            await websocket.send(outgoing[id].pop(0))
+async def sendTask(clientID, websocket):
+    while clientID in outgoing:
+        if len(outgoing[clientID]) != 0:
+            await websocket.send(outgoing[clientID].pop(0))
         else:
             await asyncio.sleep(0.05)
 
-    logPrint("sendTask for " + str(id) + " exited")
+    logPrint("sendTask for " + str(clientID) + " exited")
 
 # Registers a client, starts sendTask for it, and watches for incoming messages
 async def handler(websocket, path):
-    global nextId, clients, outgoing, incoming
+    global clients, outgoing, incoming
 
-    # Register client
-    id = nextId
-    nextId += 1
-    clients[id] = websocket
-    outgoing[id] = list()
-    incoming[id] = list()
-    sendAll("Welcome #" + str(id))
+    # Generate a clientID
+    while True:
+        clientID = random.randint(1000, 9999)
+
+        if clientID not in clients:
+            break
+
+    # Add the client to the data structures
+    clients[clientID] = websocket
+    outgoing[clientID] = list()
+    incoming[clientID] = list()
+
+    sendAll("Welcome #" + str(clientID))
+    logPrint("Client with clientID " + str(clientID) + " connected at " + path)
 
     # Start the sendTask for this socket
-    asyncio.get_event_loop().create_task(sendTask(id, websocket))
+    asyncio.get_event_loop().create_task(sendTask(clientID, websocket))
 
     # Handles incoming messages from a client
     try:
-        while id in clients:
+        while clientID in clients:
             message = await websocket.recv()
-            incoming[id].append(message)
+            incoming[clientID].append(message)
 
-            logPrint("Got message from " + str(id) + ": " + message)
+            logPrint("Got message from " + str(clientID) + ": " + message)
     except websockets.exceptions.ConnectionClosed:
         # The socket closed so remove the client
-        clients.pop(id)
-        outgoing.pop(id)
-        incoming.pop(id)
-        sendAll("Bye #" + str(id))
+        clients.pop(clientID)
+        outgoing.pop(clientID)
+        incoming.pop(clientID)
+        sendAll("Bye #" + str(clientID))
 
-    logPrint("Handler/receiveTask for " + str(id) + " exited")
+    logPrint("Handler/receiveTask for " + str(clientID) + " exited")
 
     # (When this function returns the socket dies)
 
-# Runs the frameCallback every frame and aims to hold the given frameRate
+# Runs the frameCallback every frame and aims to hold the given frame rate
 async def mainLoop(frameCallback, framesPerSecond):
+    # For frame rate targeting
     lastFrameTime = datetime.datetime.now()
     baseDelay = 1 / framesPerSecond
     delay = baseDelay
     deltas = list()
 
+    # For calculating the FPS for logging
     lastFSPLog = datetime.datetime.now()
     frameCount = 0
 
@@ -114,9 +123,9 @@ async def mainLoop(frameCallback, framesPerSecond):
         await asyncio.sleep(delay)      # (If this doesn't sleep then the other tasks can never be completed.)
 
 # Starts the sever and asyncio loop
-#    frameCallback:  The function to call every frame
-#    frameRate:      The frame rate to target
-#    debuggingLevel: 0 for none, 1 for server messages, 2 for all
+#    frameCallback:    The function to call every frame
+#    framesPerSecond:  The number of frames per second to target
+#    debuggingLevel:   0 for none, 1 for server messages, 2 for all
 def main(frameCallback, framesPerSecond, debuggingLevel):
     global logLevel
     logLevel = debuggingLevel

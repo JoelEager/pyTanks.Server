9e3e909a963a3e91d4236c6926be13c7599183d2
Joel Eager <jee7291@rit.edu>
2017-05-05 15:32:43 -0400

    Improved FPS logging

--- gameLogic/gameClock.py
+++ gameLogic/gameClock.py
@@ -39,13 +39,12 @@ async def gameClock():
     """
     Maintains a consistent frame rate as set in config.py and calls onTick() every frame
     """
-    didFPSWarn = False
-
     # For frame rate targeting
     lastFrameTime = datetime.datetime.now()
     baseDelay = 1 / config.server.framesPerSecond
     delay = baseDelay
     deltas = list()
+    minFPS = config.server.framesPerSecond
 
     # For calculating the FPS for logging
     lastFSPLog = datetime.datetime.now()
@@ -69,21 +68,17 @@ async def gameClock():
         if delay < 1 / 250:
             delay = 1 / 250
 
-        # Log low FPS warning
-        if avgDelta != 0:
-            if not didFPSWarn and 1 / avgDelta < config.server.framesPerSecond * .65:
-                    logPrint("FPS dropped to " + str(round(1 / avgDelta, 1)), 2)
-                    didFPSWarn = True
-            if didFPSWarn and 1 / avgDelta > config.server.framesPerSecond * .9:
-                    logPrint("FPS returned to " + str(round(1 / avgDelta, 1)), 2)
-                    didFPSWarn = False
-
         # Log FPS if FPS logging is enabled
+        if avgDelta != 0:
+            if 1 / avgDelta < minFPS:
+                minFPS = 1 / avgDelta
+
         if config.server.logLevel >= 1:
             frameCount += 1
 
             if (datetime.datetime.now() - lastFSPLog).total_seconds() >= config.server.fpsLogRate:
-                logPrint("FPS: " + str(frameCount / config.server.fpsLogRate), 3)
+                logPrint("FPS: avg=" + str(frameCount / config.server.fpsLogRate) + ", min=" +
+                         str(round(minFPS, 1)), 3)
                 frameCount = 0
                 lastFSPLog = datetime.datetime.now()
 
